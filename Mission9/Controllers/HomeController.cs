3acc6b2e53708bd9c86e96189e8afc454f294824
joshm <joshm@LAPTOP-PI683IVD>
2023-03-02 19:12:56 -0700

    Add project files.

---
+++ Mission9/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission9.Models;
+using Mission9.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private BookstoreProjectRepository repo;
+
+
+        public HomeController (BookstoreProjectRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BooksVM
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+
+            return View(x);
+        }
+      
+    }
+}
